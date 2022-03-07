51278f047ed2555594ebb25420c46749b8832aef
shrawaninilange <97458549+shrawaninilange@users.noreply.github.com>
2022-03-07 20:04:07 +0530

    added

---
+++ main.js
@@ -0,0 +1,12 @@
+const add=require("./add.js")
+const sub=require("./subtract.js")
+ 
+const divide = require("./divide.js")
+const mul= require("./multiplay.js")
+
+
+
+console.log(add(4,4))
+console.log(sub(4,4))
+console.log(divide(4,4))
+console.log(mul(4,4))
---
+++ multiplay.js
@@ -0,0 +1,6 @@
+const mul = (a,b)=>{
+    return a*b
+}
+
+
+module.exports=mul
